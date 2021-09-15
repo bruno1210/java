0e119f2500e4dedd3f3e356d83303d7f7ad32b62
Bruno Wilan <81261373+brunowilian@users.noreply.github.com>
2021-09-15 00:52:02 -0300

    Create tabuada.java

---
+++ tabuada.java
@@ -0,0 +1,24 @@
+
+/* Crie um programa que exiba a tabuada de um número informado pelo usuário. */
+
+public class Tabuada {
+    public static void main(String[] args) {
+       Scanner s = new Scanner (System.in);
+        int numero, n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10;
+        numero = s.nextInt();
+       
+           System.out.println("0 +" + " " + numero + " = " + (0+numero) );  
+             System.out.println("1 +" + " " + numero + " = " + (1+numero) ); 
+               System.out.println("2 +" + " " + numero + " = " + (2+numero) ); 
+                 System.out.println("3 +" + " " + numero + " = " + (3+numero) ); 
+                   System.out.println("4 +" + " " + numero + " = " + (4+numero) ); 
+                     System.out.println("5 +" + " " + numero + " = " + (5+numero) ); 
+                       System.out.println("6 +" + " " + numero + " = " + (6+numero) ); 
+                         System.out.println("7 +" + " " + numero + " = " + (7+numero) ); 
+                           System.out.println("8 +" + " " + numero + " = " + (8+numero) ); 
+                             System.out.println("9 +" + " " + numero + " = " + (9+numero) ); 
+                               System.out.println("10 +" + " " + numero + " = " + (10+numero) ); 
+                       
+           
+       }
+    }
